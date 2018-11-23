c3e2dc75518a85becc2f6687fb153b8448c7b0fc
kstandbridge <prepotency@gmail.com>
2018-11-23 11:14:57 +0000

    DirectX toolkit

--- src/DXSetup/WinMain.cpp
+++ src/DXSetup/WinMain.cpp
@@ -1,4 +1,9 @@
+#include <memory>
 #include "DXApp.h"
+#include "SpriteBatch.h"
+#include "DDSTextureLoader.h"
+#include "SimpleMath.h"
+#include "SpriteFont.h"
 
 class TestApp : public DXApp
 {
@@ -10,22 +15,38 @@ public:
 	void Update(float dt) override;
 	void Render(float dt) override;
 
+private:
+
+	std::unique_ptr<DirectX::SpriteBatch> spriteBatch;
+	std::unique_ptr<DirectX::SpriteFont> spriteFont;
+	ID3D11ShaderResourceView* m_pTexture;
+
 };
 
 TestApp::TestApp(HINSTANCE hInstance)
 	: DXApp(hInstance)
 {
-	m_AppTitle = "TUTORIAL 02 - DIRECTX ERROR HANDLING";
+	m_AppTitle = "TUTORIAL 03 - DIRECTX TOOLKIT";
 }
 
 TestApp::~TestApp()
 {
+	Memory::SafeRelease(m_pTexture);
 }
 
 bool TestApp::Init()
 {
 	if(!DXApp::Init())
 		return false;
+
+	// CREATE SPRITEBATCH OBJECTS
+	spriteBatch.reset(new DirectX::SpriteBatch(m_pImmediateContext));
+
+	// CREATE SPRITEFONT OBJECT
+	spriteFont.reset(new DirectX::SpriteFont(m_pDevice, L"assets/Arial.spritefont"));
+
+	// IMPORT TEXTURE FOR RENDERING
+	HR(DirectX::CreateDDSTextureFromFile(m_pDevice, L"assets/Mushroom.DDS", nullptr, &m_pTexture))
 
 	return true;
 }
@@ -37,6 +58,16 @@ void TestApp::Update(float dt)
 void TestApp::Render(float dt)
 {
 	m_pImmediateContext->ClearRenderTargetView(m_pRenderTargetView, DirectX::Colors::SlateGray);
+
+	spriteBatch->Begin();
+
+	 // DRAW SPRITES, FONTS ETC.
+	spriteBatch->Draw(m_pTexture, DirectX::SimpleMath::Vector2(100, 100));
+
+	// DRAW FONT
+	spriteFont->DrawString(spriteBatch.get(), L"Hello, World", DirectX::SimpleMath::Vector2(300, 300));
+
+	spriteBatch->End();
 
 	HR(m_pSwapChain->Present(0, 0));
 }
